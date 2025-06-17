dc5fc2a8ae0a7cad7873715ea5324f4785ef2981
zl <zl@gmail.com>
2025-06-17 10:32:10 +0800

    feat: .env 更新

--- types/vite-env.d.ts
+++ types/vite-env.d.ts
@@ -4,7 +4,7 @@
 // https://cn.vitejs.dev/guide/env-and-mode.html#intellisense
 interface ImportMetaEnv {
   // .env.xx 全局环境变量
-  VITE_APP_ENV: 'dev' | 'prod' | 'staging' | 'analyze'
+  VITE_APP_ENV: 'dev' | 'prod' | 'uat' | 'analyze'
   VITE_APP_NAME: string
   VITE_API_PREFIX: string
 }
