2e3d85e946ef7b3a2fc7c81f0d0002c5aafa97c9
zl <zl@gmail.com>
2025-06-17 13:55:34 +0800

    style: eslint

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,38 @@
+import { createApp } from 'vue'
+
 // MockData
 import { isProdMode } from '@/utils/appEnv.ts'
 import { setupProdMockServer } from '../mock/createProductionServer'
-import 'virtual:uno.css'
-// vant 函数组件样式
+
+import App from './App.vue'
+import router, { setupRouter } from './router'
+import { setupStore } from './stores'
+
+// vant
 import 'vant/es/toast/style'
-
 import 'vant/es/dialog/style'
-
-// vant 桌面端适配
 import '@vant/touch-emulator'
-
-// svg 雪碧图
-import 'virtual:svg-icons-register'
 
 // 注: 钉钉 webview(Chrome/69.x.x) 不支持 Promise.allsettled
 // 截止 20240308(已支持)
 // import allSettled from 'promise.allsettled';
 // allSettled.shim();
 
+// 加载 svg 图标
+import 'virtual:svg-icons-register'
+
+// UnoCSS
 // https://unocss.dev/guide/style-reset#tailwind-compat
 // 此重置基于 Tailwind 重置，减去按钮的背景颜色覆盖，以避免与 UI 框架发生冲突。请参阅链接的问题。
 import '@unocss/reset/tailwind-compat.css'
+import 'virtual:uno.css'
+
 // 移动端适应，手动转换 rem
 import './styles/flexible/flexible.js'
 
 if (isProdMode()) {
   setupProdMockServer()
 }
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import router, { setupRouter } from './router'
-import { setupStore } from './stores'
 
 async function bootstrap() {
   const app = createApp(App)
